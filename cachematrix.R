91eb13f45830dbc43db8c17f5f6df5c11ab18721
beezer13 <beezer_13@yahoo.com>
2014-06-20 16:50:19 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+### This is function which when assigned to a variable can be used
+## to set a matrix (set()), print that matrix (get()), set the value of
+## the inverse matrix (setmatrix()) and get the value of the inverse 
+## matrix (getmatrix()) using solve()
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function checks if the inverse function has already been
+## calculated, if it has it prints it, if not it calculates and
+## prints it after the message "getting cached data"
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
